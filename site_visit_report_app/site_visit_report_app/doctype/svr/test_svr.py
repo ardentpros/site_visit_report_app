07fef5bc92aab339031bd3fa38e555e68da51590
ThePAFCO Boss on TheVessel-Workstation <thepafco@outlook.com>
2024-12-04 17:15:41 +0100

    Created the doctypes
    I created all the doctypes needed for the app'

---
+++ site_visit_report_app/site_visit_report_app/doctype/svr/test_svr.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, ArdentPros Professional Services Ltd and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestSVR(FrappeTestCase):
+	pass
---
+++ site_visit_report_app/site_visit_report_app/doctype/svr_dirfi/svr_dirfi.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, ArdentPros Professional Services Ltd and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class SVRDIRFI(Document):
+	pass
